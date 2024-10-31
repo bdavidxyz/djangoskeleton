83e8ab9ecf8443ca805396d68529a43b3e798e48
David Boureau <bdavidxyz@gmail.com>
2024-10-31 09:12:35 +0100

    create myapp and create venv

--- create_django_app.py
+++ create_django_app.py
@@ -3,7 +3,9 @@ import os
 import subprocess
 import sys
 
-# Créer un environnement virtuel
-subprocess.run([sys.executable, "-m", "venv", ".venv"])
+# Create new folder myapp
+os.makedirs("myapp", exist_ok=True)
 
+# Create  "myapp/.venv"
+subprocess.run([sys.executable, "-m", "venv", "myapp/.venv"])
 
