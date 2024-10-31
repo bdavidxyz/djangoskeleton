cf8c39081de28a2b889b77a4db0de7ac6f398ba7
David Boureau <bdavidxyz@gmail.com>
2024-10-31 09:18:42 +0100

    feat: automatically create venv within myapp

--- create_django_app.py
+++ create_django_app.py
@@ -3,9 +3,16 @@ import os
 import subprocess
 import sys
 
-# Create new folder myapp
+# Create a new folder "myapp"
 os.makedirs("myapp", exist_ok=True)
 
-# Create  "myapp/.venv"
+# Create a virtual environment in "myapp/.venv"
 subprocess.run([sys.executable, "-m", "venv", "myapp/.venv"])
 
+# Activate the virtual environment
+if os.name == 'nt':  # For Windows
+    activation_cmd = ["cmd", "/c", "myapp\\.venv\\Scripts\\activate.bat"]
+else:  # For Unix or macOS
+    activation_cmd = ["source", "myapp/.venv/bin/activate"]
+
+subprocess.run(activation_cmd, shell=True)
